b1d24e090408bf521adbf3087241d8d4908226b6
colin mcgovern <colinmcg@udel.edu>
2023-05-03 15:45:15 -0400

    finished video 20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/product')
+  }
+
   return (
     <>
       <h1>Home page</h1>
+      <Link href='/blog'>
+        Blog
+      </Link>
+      <button onClick={handleClick}>
+        Place order
+      </button>
     </>
   )
 }
