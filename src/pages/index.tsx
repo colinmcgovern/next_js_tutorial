97c7330e669f42203868ccda834872de1f66ad1e
colin mcgovern <colinmcg@udel.edu>
2023-05-11 18:06:09 -0400

    video 22 completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,11 @@ function Home() {
   return (
     <>
       <h1>Home page</h1>
-      <Link href='/blog'>
-        Blog
+      <Link href='/users'>
+       User
+      </Link>
+      <Link href='/posts'>
+        Posts
       </Link>
       <button onClick={handleClick}>
         Place order
